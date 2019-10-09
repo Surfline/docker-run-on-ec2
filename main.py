3bc6d5aace1ee8e6c912ad14cecf074965580491
Stephen Link <stephenlink@SLink-MBP.local>
2019-10-09 15:41:26 -0700

    updated README and added split on comma

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
 
     name = os.environ['NAME']
     key_name = f'{name}-{uuid4()}'
-    launch_template_names = os.environ['LAUNCH_TEMPLATE_NAMES'].split()
+    launch_template_names = os.environ['LAUNCH_TEMPLATE_NAMES'].split(',')
     subnet_id = os.environ['SUBNET_ID']
     command = ' '.join(sys.argv[1:])
 
